a6762638a5e01e03f1535a73a8669d5efd8bbc30
Isaac Elliott <isaace71295@gmail.com>
2024-03-23 17:11:34 +1000

    Add Nix derivation for program

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix.url = "github:cargo2nix/cargo2nix";
+    rust-overlay.follows = "cargo2nix/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
             rust-overlay.overlays.default
+            cargo2nix.overlays.default
           ];
         };
 
@@ -29,11 +32,25 @@
                   "rust-analyzer"
                 ];
               })
+              cargo2nix.packages.${system}.cargo2nix
 
               pkgs.pkg-config
               pkgs.openssl.dev
             ];
           };
+
+        packages = rec {
+          git-ustash =
+            let
+              rustPkgs = pkgs.rustBuilder.makePackageSet {
+                inherit rustVersion;
+                packageFun = import ./Cargo.nix;
+              };
+            in
+            rustPkgs.workspace.git-ustash {};
+
+          default = git-ustash;
+        };
       }
     );
 }
